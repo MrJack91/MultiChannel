e20926756d92a4ee576c2342edf2b08a7c6df929
Marco <macrozone@gmail.com>
2013-05-22 19:36:28 +0200

    bla

---
+++ src/ch/zhaw/mima/App.java
@@ -0,0 +1,36 @@
+package ch.zhaw.mima;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		App app = new App();
+		app.startup();
+
+	}
+
+	public void startup() {
+		
+		JFrame mainFrame = new JFrame();
+		
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		
+		
+		
+	
+		
+	}
+
+}
